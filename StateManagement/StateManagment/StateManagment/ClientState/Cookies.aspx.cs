49f57a7f2ba5fd3d3cb2d75f38592c4867443938
Konstantino <papalamus@yandex.ru>
2014-06-05 16:05:00 +0400

    Missed Cookies added

---
+++ StateManagement/StateManagment/StateManagment/ClientState/Cookies.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StateManagment.Pages
+{
+    public partial class Cookies : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void Page_LoadComplete(object sender, EventArgs e)
+        {
+            if (Request.Cookies["MyCookie"] != null)
+            {
+                SavedValueTextBox.Text = Request.Cookies["MyCookie"]["Value"];
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            HttpCookie cookie = new HttpCookie("MyCookie");
+            cookie["Value"] = SavingValueTextBox.Text;
+            Request.Cookies.Add(cookie); 
+        }
+    }
+}
